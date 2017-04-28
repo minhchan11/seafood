d6cb3fdc7e94a2a575a9515445bf3a52a2e4bea8
Minh Phuong <mphuong@kent.edu>
2017-04-28 12:54:44 -0700

    edit user passed

---
+++ src/Seafood/Controllers/RolesController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Seafood.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Seafood.Controllers
+{
+    [Authorize]
+    public class RolesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public RolesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _db.Roles.ToList();
+            return View(roles);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(IdentityRole role)
+        {
+            _db.Roles.Add(role);
+            _db.SaveChanges();
+            ViewBag.ResultMessage = "Role created successfully";
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(string roleId)
+        {
+            var thisRole = _db.Roles.Where(r => r.Id.Equals(roleId, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            return View(thisRole);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(IdentityRole role)
+        {
+            try
+            {
+                var thisRole = _db.Roles.Where(r => r.Id.Equals(role.Id, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+                _db.Roles.Attach(thisRole);
+                thisRole.Name = role.Name;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return View(role);
+            }
+        }
+
+        public IActionResult Delete(string RoleName)
+        {
+            var thisRole = _db.Roles.Where(r => r.Name.Equals(RoleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            return View(thisRole);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteRole(string RoleName)
+        {
+            var thisRole = _db.Roles.Where(r => r.Name.Equals(RoleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            _db.Roles.Remove(thisRole);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Manage()
+        {
+            ViewBag.Roles = new SelectList(_db.Roles, "Id", "Name");
+            ViewBag.Users = new SelectList(_db.Users, "Id", "Name");
+            return View();
+        }
+
+
+    }
+}
